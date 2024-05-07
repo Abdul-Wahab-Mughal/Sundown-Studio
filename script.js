0de879b33ea18c4b994a667f5f12588cf8f4b6f3
Abdul-Wahab-Mughal <abdulwahabmughal10@gmail.com>
2024-05-07 16:27:42 +0500

    updated 5 / 7

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * loader
@@ -15,12 +14,23 @@ function loader() {
  * Featured
  *
  */
+var cardImg = [
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b16f4_Copy%2520of%2520Nike_Soho_50th_SU22_FL1_6176-p-1600.webp",
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b163b_Copy-of-IMG_1180-p-1600.webp",
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b1698_Copy%2520of%2520Nike_Soho_Play-New-Kids_10-27-21_1950-p-1600.webp",
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b163d_Copy%2520of%2520DSC04086-p-1600.webp",
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b16c5_Copy%2520of%2520Nike_Soho_AMD21_0781_LORES-p-1600.webp",
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b1733_Nike_HOI_50th_SU22_FL1_5388.webp",
+  "https://assets-global.website-files.com/64d3dd9edfb41666c35b15d4/64d3dd9edfb41666c35b170d_AM704059-p-1600.webp",
+];
 function Featured() {
   let it = document.querySelectorAll(
     ".featured-project .list .item .card-image"
   );
+  let cardTop = document.querySelectorAll(".featured-project .list .item");
 
   if (document.body.clientWidth <= 550) {
+    document.getElementById("card-display").style = "display: none";
     document.querySelector(".design-box").style = "min-height: 1000px;";
     for (i of it) {
       i.style = `
@@ -35,21 +45,57 @@ function Featured() {
       left: 0;`;
     }
   } else {
+    document.getElementById("card-display").style = "display: block";
     document.querySelector(".design-box").style = "min-height: 100%;";
     for (i of it) {
       i.style = `height: 100% !important; 
     position: fixed !important`;
 
-      i.querySelector("img").style = `
-    position: absolute !important;
-    width: 300px !important; 
-    height: 450px !important;
-    top: 25%; 
-    left: 50%;`;
+      //   i.querySelector("img").style = `
+      // position: absolute !important;
+      // width: 300px !important;
+      // height: 450px !important;
+      // top: 25%;
+      // left: 50%;`;
+      i.querySelector("img").style = ` display:none`;
     }
+
+    document.getElementById("card-display").innerHTML = cardImg
+      .map((e, i) => {
+        let ctTotal = cardTop[i].offsetTop - 75;
+        return `<div style="display:none">
+          <img
+            class="m-3 m-sm-0 object-fit-cover rounded-4 position-absolute start-50 z-3 pe-none "
+            src="${e}"
+            style="top: ${ctTotal}px; width: 300px; height: 400px"
+            alt=""
+          />
+        </div>`;
+      })
+      .join("");
+
+    let ci = document.querySelectorAll("#card-display div");
+    // setTimeout(() => {
+    for (c in cardTop) {
+      if (c <= 6) {
+        cardTop[c].id = c;
+        cardTop[c].addEventListener("mouseenter", (e) => {
+          ci[e.target.id].style = "display: block";
+        });
+        cardTop[c].addEventListener("mouseleave", (e) => {
+          ci[e.target.id].style = "display: none";
+        });
+      }
+    }
+    // }, 2000);
   }
 }
 
+/**
+ *
+ * Design Card
+ *
+ */
 function DesignCard() {
   var des = ["design", "project", "execution"];
   for (i of document.querySelectorAll(".design-box .card-item")) {
